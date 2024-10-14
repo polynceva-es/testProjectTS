f5b3cf03c6a1ee5cec6cd0b2ebd06618aa289768
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2024-10-14 11:16:38 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/testProjectTS",
+    // basePath: "/testProjectTS",
     output: process.env.NODE_ENV === 'production' ? 'export' : undefined, // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist/`.
     // basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
