75a1304f64ff253ebce71851ceee513174b16506
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2024-10-14 11:10:28 +0300

    fix ghpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/testProjectTS",
     output: process.env.NODE_ENV === 'production' ? 'export' : undefined, // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist/`.
     // basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
   }
    
   export default nextConfig
+
+
+  
+  // "homepage": "https://polynceva-es.github.io/testProjectTS/",
